13887cc298ac113e6722fe24c32624dfb6ec583f
jozsa <allisonjoyweiner@gmail.com>
2019-05-09 19:28:36 +0000

    Subscriber count for a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module has one function: number_of_subscribers(subreddit).
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    # URL and headers to make API requests from
+    url = 'https://reddit.com/r'
+
+    # Subscriber count information
+    subscriber_count = requests.get('{}/{}/about.json'.format(url, subreddit),
+                                    headers={'User-Agent': 'Mozilla/5.0'}) \
+        .json().get('data').get('subscribers')
+
+    if not subscriber_count:
+        subscriber_count = 0
+
+    return subscriber_count
